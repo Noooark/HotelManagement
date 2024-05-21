12224cd1bb8f3b19d9d70f43338fde8635d40daf
Khôi Lê <acekhoi2005@gmail.com>
2024-05-21 10:49:45 +0700

    update

--- HotelManagement/src/DAO/DAOInterface.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-import java.util.ArrayList;
-
-public interface DAOInterface<T> {
-    public int Login(T t);
-    public int createaccount(T t);
-    public ArrayList<T> selectAll();
-}
